338b948c2f03edaa191f3f88c64bd4297621a785
Alex sheffcer <a.sheffcer@gmail.com>
2020-02-06 16:29:16 +0200

    added swiper in roomSlider

--- src/components/RoomSlider.vue
+++ src/components/RoomSlider.vue
@@ -2,13 +2,8 @@
 <div class="room-slider">
     <div class="room-slider__inner">
     <h3 class="room-slider__header">Номера</h3>
-        <!-- <carousel -->
-        <!-- class="owl-carousel owl-theme review__grid"
-        :autoplay="true" :nav="false"
-        :responsive="{0:{items:1,nav:false},320:{items:1,nav:false},768:{items:2,nav:true}, 1170:{items:3,nav:true}}"
-        > -->
-
-            <!-- <div class="room-slider__item" v-for='room in rooms' v-bind:key='room.id'>
+        <swiper :options="swiperOption" ref="swiperOption">
+        <swiper-slide class="room-slider__item"  v-for='room in rooms' v-bind:key='room.id'>
             <div class="room-slider__item-wrap">
             <picture class="room-slider__item-img">
             <source media="(min-width: 1366px)" v-bind:srcset="room.img_link_desktop">
@@ -17,13 +12,15 @@
             </picture>
             <div class="room-slider__content">
             <h2 class="room-slider__title">{{room.title}}</h2>
-
+               <ul class="list-bullet">
+                <li v-for="(list, index) in room.lists" :key="index" >{{list}}</li>
+                </ul>
             <a href="#" class="btn  btn--slider">Забронировать</a>
             </div>
             </div>
-            </div> -->
-        <!-- </carousel> -->
-    <div id="rooms" class="owl-carousel owl-theme room-slider__grid">
+        </swiper-slide>
+        </swiper>
+    <!-- <div id="rooms" class="owl-carousel owl-theme room-slider__grid">
         <div class="room-slider__item">
         <div class="room-slider__item-wrap">
             <picture class="room-slider__item-img">
@@ -81,7 +78,7 @@
             </div>
         </div>
         </div>
-    </div>
+    </div> -->
     </div>
     </div>
 
@@ -89,19 +86,37 @@
 
 <script>
 // import carousel from 'vue-owl-carousel'
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
 export default {
 name: 'RoomSlider',
 components: {
     // carousel
+    swiper,
+    swiperSlide
 },
+
 data () {
     return {
     sliderValue: 3,
+        swiperOption: {
+    // effect: 'coverflow',
+    // grabCursor: true,
+    // centeredSlides: true,
+    loop: true,
+    autoplay: true,
+    slidesPerView: 1,
+    pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            },
+        },
     rooms: [
         {
             id: 1,
             title: 'Эконом',
-            list: ['Площадь - 0,90 м2', 'Размеры (ШхГхВ) 90х100х180 см', 'Цена за сутки: 200₽'],
+            lists: ['Площадь - 0,90 м2', 'Размеры (ШхГхВ) 90х100х180 см', 'Цена за сутки: 200₽'],
             img_link_mobile: 'img/room_01.png',
             img_link_tablet: 'img/room_01-tablet.png',
             img_link_desktop: 'img/room_01-desktop.png',
@@ -109,7 +124,7 @@ data () {
         {
             id: 2,
             title: 'Эконом плюс',
-            list: ['Площадь - 1,13 м2', 'азмеры (ШхГхВ) 100х125х180 см', 'Цена за сутки: 250₽'],
+            lists: ['Площадь - 1,13 м2', 'азмеры (ШхГхВ) 100х125х180 см', 'Цена за сутки: 250₽'],
             img_link_mobile: 'img/room_02.png',
             img_link_tablet: 'img/room_02-tablet.png',
             img_link_desktop: 'iimg/room_02-desktop.png',
@@ -117,7 +132,7 @@ data () {
         {
             id: 3,
             title: 'Сьют',
-            list: ['Площадь - 1,56 м2', 'Размеры (ШхГхВ) 125х125х180 см', 'Цена за сутки: 350₽'],
+            lists: ['Площадь - 1,56 м2', 'Размеры (ШхГхВ) 125х125х180 см', 'Цена за сутки: 350₽'],
             img_link_mobile: 'img/room_03.png',
             img_link_tablet: 'img/room_03-tablet.png',
             img_link_desktop: 'img/room_03-desktop.png',
@@ -125,6 +140,15 @@ data () {
     ],
     }
 },
+computed: {
+    swiper() {
+        return this.$refs.swiperOption.swiper
+    }
+},
+mounted () {
+    // console.log('this is current swiper instance object', this.swiper)
+    this.swiper.slideTo(1, 2000, true)
+}
 }
 </script>
 
